8684dbc1dfd2dea5e46fa15a38bf9ffcd177146e
bernaluc <bernaluc@oregonstate.edu>
2021-12-13 20:21:32 -0700

    AoC day 3 still missing conversion to boolean and then decimal to get final answer. Part 2 still missing

--- AoC_Day3.py
+++ AoC_Day3.py
@@ -1,14 +1,40 @@
 import re
 
-with open('input_day2.txt') as f:
+with open('input_day3.txt') as f:
     lines = f.readlines()
 
 data = []
+bools = []
 for line in lines:
     data.append(line.strip())
 
-    num_1 = re.findall('1', line)
-    num_2 = re.findall('0', line)
+for line in data:
+    for num in line:
+        bools.append(num)
 
-    print(num_1)
-    print(num_2)
+counts = [0]*len(data[0])
+for line in data:
+    pos = 0
+    for num in line:
+        if num == "1":
+            counts[pos] += 1
+        pos +=1
+
+print(counts)
+
+zeros = []
+ones = []
+
+for i in counts:
+    if i > int(len(data)/2):
+        zeros.append(1)
+        ones.append(0)
+    else:
+        zeros.append(0)
+        ones.append(1)
+
+print(zeros)
+print(ones)
+
+
+
